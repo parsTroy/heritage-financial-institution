e02c7ca6bc5029cb402bb947e55f56f96738f101
parsTroy <troyparsons@hotmail.com>
2024-05-26 18:29:49 -0400

    Add animated bank balance

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,5 +1,7 @@
 import { formatAmount } from "@/lib/utils";
 import React from "react";
+import CountUp from "react-countup/build/CountUp";
+import AnimatedCounter from "./AnimatedCounter";
 
 const TotalBalanceBox = ({
   accounts = [],
@@ -16,7 +18,7 @@ const TotalBalanceBox = ({
           <p className="total-balance-label">Total Current Balance</p>
 
           <p className="total-balance-amount flex-center gap-2">
-            {formatAmount(totalCurrentBalance)}
+            <AnimatedCounter amount={totalCurrentBalance} />
           </p>
         </div>
       </div>
