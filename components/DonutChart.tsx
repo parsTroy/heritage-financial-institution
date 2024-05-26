b8017dc3b99e31800d96da8ae738fa22c7f2d4df
parsTroy <troyparsons@hotmail.com>
2024-05-26 18:39:29 -0400

    Add donut chart base

--- components/DonutChart.tsx
+++ components/DonutChart.tsx
@@ -6,8 +6,19 @@ import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const DonutChart = () => {
-  return <Doughnut data={[]} />;
+const DonutChart = ({ accounts }: DoughnutChartProps) => {
+  const data = {
+    datasets: [
+      {
+        label: "Banks",
+        data: [1523, 2642, 8152],
+        backgroundColor: ["#0747b6", "#2265d8", "#2f91fa"],
+      },
+    ],
+    labels: ["Chase", "Wells Fargo"],
+  };
+
+  return <Doughnut data={data} />;
 };
 
 export default DonutChart;
--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -2,6 +2,7 @@ import { formatAmount } from "@/lib/utils";
 import React from "react";
 import CountUp from "react-countup/build/CountUp";
 import AnimatedCounter from "./AnimatedCounter";
+import DonutChart from "./DonutChart";
 
 const TotalBalanceBox = ({
   accounts = [],
@@ -10,16 +11,19 @@ const TotalBalanceBox = ({
 }: TotalBalanceBoxProps) => {
   return (
     <section className="total-balance">
-      <div className="total-balance-chart">{/* Donut Chart */}</div>
+      <div className="total-balance-chart">
+        {/* Donut Chart */}
+        <DonutChart accounts={accounts} />
+      </div>
 
       <div className="flex flex-col gap-6">
         <h2 className="header-2">Bank Accounts: {totalBanks}</h2>
         <div className="flex flex-col gap-2">
           <p className="total-balance-label">Total Current Balance</p>
 
-          <p className="total-balance-amount flex-center gap-2">
+          <div className="total-balance-amount flex-center gap-2">
             <AnimatedCounter amount={totalCurrentBalance} />
-          </p>
+          </div>
         </div>
       </div>
     </section>
