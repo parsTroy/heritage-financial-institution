94277c6c3dbe69ef8e0616b1cb8de960918aeb82
parsTroy <troyparsons@hotmail.com>
2024-05-28 12:36:40 -0400

    Add Right sidebar base

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Troy" };
+  const loggedIn = {
+    firstName: "Troy",
+    lastName: "Parsons",
+    email: "troy@me.com",
+  };
 
   return (
     <section className="home">
@@ -22,7 +27,10 @@ const Home = () => {
             totalCurrentBalance={4291.23}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSideBar user={loggedIn} transactions={[]} banks={[]} />
     </section>
   );
 };
